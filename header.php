0e7c15a79647da5c124acc5cd9f77ed2a59ef1c5
mft6300 <mft6300@gmail.com>
2023-02-14 14:05:30 -0600

    Landing Page initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FUEL APPLICATION</title>
+</head>
+<body>
+  <div class='centered-div'>
+    <header>
+      <nav>
+       <div>
+        <h1>temp</h1>
+       </div>
+      </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+  include_once 'header.php';
+?>
+<div class='title'>
+  <h1>FUEL APPLICATION - GROUP 14</h1>
+</div>
