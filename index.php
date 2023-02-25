c7796b6ecd5ac41569c0f552bb4c57622c00e47a
ProBroXP <probroxp@gmail.com>
2023-02-24 21:21:14 -0600

    index.php to bootstrap

--- index.php
+++ index.php
@@ -1,40 +1,57 @@
 <?php
 include_once 'header.php';
+include_once 'navbar.php';
 ?>
-<div class='welcome'>
-  <h1>FUEL APPLICATION - HOMEPAGE</h1>
+<div class="container d-flex justify-content-center align-items-center">
+	<div class="card" style="width: 25rem">
+		<div class="card-body">
+			<div class="d-flex justify-content-center">
+				<h5 class="card-title">Login</h5>
+			</div>
+			<form>
+				<div class="form-outline mb-4">
+					<input type="email" name="username" id="username" class="form-control" placeholder="Email address" required/>
+				</div>
+				
+				<div class="form-outline mb-4">
+					<input type="password" name="password" id="password"  class="form-control" placeholder="Password" required/>
+				</div>
+				
+				<div class="row mb-4">
+					<div class="col d-flex justify-content-center">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="rememberMe" checked />
+						<label class="form-check-label" for="rememberMe"> Remember me </label>
+					</div>
+					</div>
+				
+					<div class="col">
+					<a href="#!">Forgot password?</a>
+					</div>
+				</div>
+				
+				<div class="d-grid gap-2">
+					<button type="button" class="btn btn-primary mb-4">Sign in</button>
+				</div>
+				<?php
+				if (isset($_GET["error"])) {
+				if ($_GET["error"] === "emptyinput") {
+					echo "<p>Fill in all fields.</p>";
+				} else if ($_GET["error"] === "wronglogin") {
+					echo "<p>Wrong login information. Try again.</p>";
+				} else if ($_GET["error"] === "stmtfailed") {
+					echo "<p>Something went wrong. Try again.</p>";
+				}
+				}
+				?>
+				<div class="text-center">
+					<p>Not a member? <a href="#!">Register</a></p>
+				</div>
+				</form>
+		</div>
+	</div>
 </div>
-<div>
-  <div class='login-form'>
-    <h2>Log In</h2>
-    <form action='/includes/login.inc.php' method='post'>
-      <div class='form-element'>
-        <label for='username'>Enter Username or Email</label>
-        <input type='text' name='username' id='username' placeholder='Username or Email' required />
-      </div>
-      <div class='form-element'>
-        <label for='password'>Enter Password</label>
-        <input type='password' name='password' id='password' placeholder='Password' required />
-      </div>
-      <div class='submit-btn'>
-        <button type='submit' name='submit'>Log In</button>
-      </div>
-      <div>
-        <a href='signup.php'>New User? Sign Up from here!</a>
-      </div>
-    </form>
-    <?php
-    if (isset($_GET["error"])) {
-      if ($_GET["error"] === "emptyinput") {
-        echo "<p>Fill in all fields.</p>";
-      } else if ($_GET["error"] === "wronglogin") {
-        echo "<p>Wrong login information. Try again.</p>";
-      } else if ($_GET["error"] === "stmtfailed") {
-        echo "<p>Something went wrong. Try again.</p>";
-      }
-    }
-    ?>
-  </div>
-  <?php
-  include_once 'footer.php';
-  ?>
+<?php
+include_once 'footer.php';
+?>
+
